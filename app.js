646a1e638c63ae0b9bf271c0906033a00e50afdb
MiccWan <michaelwan0906@hotmail.com>
2018-08-13 03:06:13 +0000

    add app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Require package
+var express = require('express')
+var app = require('express')()
+var server = require('http').Server(app)
+
+// Default settings
+var config = require('./config.json')
+var port = config.port
+
+// app.use for public folder
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/assets', express.static(__dirname + '/assets'))
+
+// Routers
+app.get('/', function(req, res){
+	console.log("[Connect] Get request to '/'")
+	res.sendFile(__dirname+'index.html')
+})
+
+// Listen on port
+server.listen(port, function(){
+	console.log("Server is running at port", port)
+})
