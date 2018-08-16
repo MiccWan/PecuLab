469ce44fb6456c15fc1df8c70616d8c6562ac703
MiccWan <michaelwan0906@hotmail.com>
2018-08-16 13:11:24 +0800

    fix auto click all-filter

--- public/js/custom.js
+++ public/js/custom.js
@@ -118,7 +118,6 @@ jQuery(document).ready(function($) {
 		$container.isotope({ filter: selector });
 		return false;
 	});
-	$('#filters a').eq(0).click()
 
 	// flexslider main
 	$('#main-flexslider').flexslider({						
